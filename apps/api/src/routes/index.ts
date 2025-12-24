ae96b89dee7a6dd984e9ef1ce811519837cf8d2b
Son Pham <son061203@gmail.com>
2025-12-24 14:51:33 +0700

    fix: remove anime route

--- apps/api/src/routes/index.ts
+++ apps/api/src/routes/index.ts
@@ -3,5 +3,5 @@
  * Import all routes from this file for convenience
  */
 
-export { animeRoutes } from './anime.js';
+// export { animeRoutes } from './anime.js';
 export { songRoutes } from './song.js';
